c91456575ad6f17a17f5f21af97d3281b8f3d62f
Cecchy2 <dariocecchinato@gmail.com>
2024-06-18 11:08:38 +0200

    cambiato album da fetch

--- AlbumPage.js
+++ AlbumPage.js
@@ -2,11 +2,14 @@ const params = new URLSearchParams(window.location.search);
 
 const id = params.get("....id");
 
-//ci estrarra' il valore corrispondente alla chiave che avremo dato
-
-//al caricamento della pagina chiediamo al server  di darci i dati della risorsa con id che triviamo nell'HTTP
 window.addEventListener("DOMContentLoaded", function () {
-  this.fetch("https://striveschool-api.herokuapp.com/api/deezer/album/75621062")
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/album/44941731", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "163c72cf37msh7fb90cec4c02a73p1390b4jsn4594dd70494e",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
     .then((resp) => {
       if (resp.ok) {
         return resp.json();
@@ -15,31 +18,49 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     })
     .then((albumObj) => {
+      const date = albumObj.release_date;
+      console.log(date);
+      const newDate = new Date(date);
+      console.log(newDate);
+
+      const year = newDate.getFullYear();
+      console.log(year);
+
       console.log(albumObj);
       const imgCurrentAlbum = document.getElementById("albumImg");
       const titleAlbum = document.getElementById("albumTitle");
       const artistAlbum = document.getElementById("albumArtist");
       imgCurrentAlbum.src = albumObj.cover;
       titleAlbum.innerText = albumObj.title;
-      artistAlbum.innerText = albumObj.artist.name;
+      artistAlbum.innerText = albumObj.artist.name + " " + year + " " + albumObj.nb_tracks + " brani,";
 
       const tracksTable = document.getElementById("tracksTable").querySelector("tbody");
-      albumObj.tracks.data.forEach((track) => {
+      tracksTable.innerHTML = "";
+      albumObj.tracks.data.forEach((track, index) => {
         const trackRow = document.createElement("tr");
 
-        const trackNameCell = document.createElement("td");
-        trackNameCell.innerText = track.title;
+        // Numero Cella
+        const trackNumberCell = document.createElement("th");
+        trackNumberCell.scope = "row";
+        trackNumberCell.innerText = index + 1;
+        trackRow.appendChild(trackNumberCell);
 
+        // Titolo
+        const trackTitleCell = document.createElement("td");
+        trackTitleCell.innerText = track.title;
+        trackRow.appendChild(trackTitleCell);
+
+        // Track Play
+        const trackPlaysCell = document.createElement("td");
+        trackPlaysCell.innerText = track.rank;
+        trackRow.appendChild(trackPlaysCell);
+
+        // Durata
         const trackDurationCell = document.createElement("td");
         const minutes = Math.floor(track.duration / 60);
         const seconds = track.duration % 60;
-        trackDurationCell.innerText = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-
-        const trackPlaysCell = document.createElement("td");
-        trackPlaysCell.innerText = track.rank;
-        trackRow.appendChild(trackNameCell);
+        trackDurationCell.innerText = `${minutes}:${seconds}`;
         trackRow.appendChild(trackDurationCell);
-        trackRow.appendChild(trackPlaysCell);
 
         tracksTable.appendChild(trackRow);
       });
