f29b8cf84373f2a80fe7e3d77bf20b221f96cb2a
Cecchy2 <dariocecchinato@gmail.com>
2024-06-20 14:47:14 +0200

    eliminata funzione scroll che bloccava albumPage

--- home.js
+++ home.js
@@ -15,7 +15,7 @@ const albumsStorici = [96126, 47131362, 12047952, 104660202, 96001912, 1262014];
 const options = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "c1be13bc83msh01ed86504ac789ap14b677jsn4a8378e3cb43",
+    "x-rapidapi-key": "dfd3925d0amshafe029754eb961ap17f037jsn18e065c48a37",
     /* "x-rapidapi-key": "c1be13bc83msh01ed86504ac789ap14b677jsn4a8378e3cb43", */
     "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
   },
