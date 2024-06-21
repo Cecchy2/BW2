387b303adfceb21b4334933333db2f38086e29ad
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-06-21 11:48:33 +0200

    sistemato la randomizzazione del tasto play sul tasto album

--- home.js
+++ home.js
@@ -4,8 +4,9 @@ const artistiPopolari = [13, 66, 7543848, 12246, 384236, 1188];
 // array di artisti presi in modo casuale e messi come estensione del url per accedere alle top 50 canzoni
 // const indexArtists = [1, 2, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 25, 21, 22, 24, 26, 27, 28, 29, 30];
 const albumAnnunci = [
-  400319947, 108938, 423368, 159826232, 10613684, 194246202, 8178950, 273367132, 591398592, 9410100, 303950837, 581531012, 580186491, 554390622, 597941372, 560398332, 6240279, 96126, 47131362,
-  12047952, 104660202, 96001912, 1262014, 299484812,
+  400319947, 108938, 423368, 159826232, 10613684, 194246202, 8178950, 273367132, 591398592, 9410100, 303950837,
+  581531012, 580186491, 554390622, 597941372, 560398332, 6240279, 96126, 47131362, 12047952, 104660202, 96001912,
+  1262014, 299484812,
 ];
 // array di album artista preferito (eminem)
 const albumsArtistaPref = [103248, 119606, 7090505, 595243, 72000342, 125748];
@@ -45,7 +46,7 @@ const creaCards = (artist, container, index) => {
   const img = document.createElement("img");
   img.className = "bd-placeholder-img card-img-top object-fit-cover rounded-circle";
   img.setAttribute("src", artist.picture_big);
-  img.addEventListener("click", event => {
+  img.addEventListener("click", (event) => {
     window.location.assign("./artists.html?artistId=" + artist.id);
   });
 
@@ -53,7 +54,8 @@ const creaCards = (artist, container, index) => {
   btnPlay.type = "button";
   btnPlay.setAttribute("style", "width: 50px; height:50px");
   // btnPlay.href = "./back-office.html?productId=" + songs.data[i]._id;
-  btnPlay.className = "btn btn-success rounded-circle  position-absolute  bottom-0 end-0 me-2 mb-2 d-flex align-items-center justify-content-center d-none ";
+  btnPlay.className =
+    "btn btn-success rounded-circle  position-absolute  bottom-0 end-0 me-2 mb-2 d-flex align-items-center justify-content-center d-none ";
   btnPlay.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-play-fill" viewBox="0 0 16 16">
     <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/></svg>`;
 
@@ -66,7 +68,7 @@ const creaCards = (artist, container, index) => {
     btnPlay.classList.add("d-none");
   }
 
-  btnPlay.addEventListener("click", event => {
+  btnPlay.addEventListener("click", (event) => {
     console.log("Button clicked");
     console.log(arrayArtist);
     const imgArtistaAlbum = document.getElementById("imgArtistaAlbum");
@@ -99,7 +101,7 @@ const creaCards = (artist, container, index) => {
   const h5 = document.createElement("h5");
   h5.innerText = artist.name;
   h5.className = "fs-5 text-truncate ";
-  h5.addEventListener("click", event => {
+  h5.addEventListener("click", (event) => {
     window.location.assign("./artists.html?artistId=" + artist.id);
   });
   const type = document.createElement("p");
@@ -136,7 +138,7 @@ const creaCards = (artist, container, index) => {
 const cardsArtist = (arrArtists, container) => {
   arrArtists.forEach((artist, index) => {
     fetch("https://deezerdevs-deezer.p.rapidapi.com/artist/" + artist, options)
-      .then(resp => {
+      .then((resp) => {
         if (resp.ok) {
           // restituiamo il dato convertito in array da JSON
           return resp.json();
@@ -144,11 +146,11 @@ const cardsArtist = (arrArtists, container) => {
           throw `Errore ${resp.status} : ${resp.statusText} `;
         }
       })
-      .then(artistResp => {
+      .then((artistResp) => {
         // creo cards con le canzoni del url inserito e le appendo al container dato come parametro
         creaCards(artistResp, container, index);
       })
-      .catch(err => alert(err));
+      .catch((err) => alert(err));
   });
 };
 
@@ -156,7 +158,7 @@ const cardsArtist = (arrArtists, container) => {
 const cardsAlbum = (arrAlbums, container) => {
   arrAlbums.forEach((album, index) => {
     fetch("https://deezerdevs-deezer.p.rapidapi.com/album/" + album, options)
-      .then(resp => {
+      .then((resp) => {
         if (resp.ok) {
           // restituiamo il dato convertito in array da JSON
           return resp.json();
@@ -164,7 +166,7 @@ const cardsAlbum = (arrAlbums, container) => {
           throw `Errore ${resp.status} : ${resp.statusText} `;
         }
       })
-      .then(album => {
+      .then((album) => {
         // creo album con l'url inserito e le appendo al container dato come parametro
         const moTifrego = album;
         const row = document.querySelector(container);
@@ -182,14 +184,15 @@ const cardsAlbum = (arrAlbums, container) => {
         const img = document.createElement("img");
         img.className = "bd-placeholder-img card-img-top object-fit-cover ";
         img.setAttribute("src", album.cover_big);
-        img.addEventListener("click", event => {
+        img.addEventListener("click", (event) => {
           window.location.assign("./AlbumPage.html?albumId=" + album.id);
         });
         const btnPlay = document.createElement("a");
         btnPlay.type = "button";
         btnPlay.setAttribute("style", "width: 50px; height:50px");
         // btnPlay.href = "./back-office.html?productId=" + songs.data[i]._id;
-        btnPlay.className = "btn btn-success btnPlay  rounded-circle   position-absolute  bottom-0 end-0 me-2 mb-2 d-flex align-items-center justify-content-center d-none ";
+        btnPlay.className =
+          "btn btn-success btnPlay  rounded-circle   position-absolute  bottom-0 end-0 me-2 mb-2 d-flex align-items-center justify-content-center d-none ";
         btnPlay.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-play-fill" viewBox="0 0 16 16">
           <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/></svg>`;
 
@@ -201,7 +204,8 @@ const cardsAlbum = (arrAlbums, container) => {
         function out() {
           btnPlay.classList.add("d-none");
         }
-        btnPlay.addEventListener("click", event => {
+
+        btnPlay.addEventListener("click", (event) => {
           console.log("Button clicked");
           console.log(moTifrego);
           const imgArtistaAlbum = document.getElementById("imgArtistaAlbum");
@@ -210,6 +214,7 @@ const cardsAlbum = (arrAlbums, container) => {
           const image = document.createElement("img");
           const h5 = document.createElement("h5");
           if (imgArtistaAlbum.firstChild && imgArtistaAlbum.firstChild.src) {
+            const indexPazzo = Math.floor(Math.random() * 16);
             imgArtistaAlbum.innerHTML = "";
             footerTitolo.innerHTML = "";
             footerArtista.innerHTML = "";
@@ -217,14 +222,15 @@ const cardsAlbum = (arrAlbums, container) => {
             imgArtistaAlbum.appendChild(image);
             footerTitolo.appendChild(h5);
             imgArtistaAlbum.classList.add("me-2");
-            h5.innerText = moTifrego.title;
+            h5.innerText = moTifrego.tracks.data[indexPazzo].title;
             footerArtista.innerText = moTifrego.artist.name;
           } else {
+            const indexPazzo = Math.floor(Math.random() * 16);
             image.src = moTifrego.cover_small;
             imgArtistaAlbum.appendChild(image);
             footerTitolo.appendChild(h5);
             imgArtistaAlbum.classList.add("me-2");
-            h5.innerText = moTifrego.title;
+            h5.innerText = moTifrego.tracks.data[indexPazzo].title;
             footerArtista.innerText = moTifrego.artist.name;
           }
         });
@@ -234,12 +240,13 @@ const cardsAlbum = (arrAlbums, container) => {
         const h5 = document.createElement("h5");
         h5.innerText = album.title;
         h5.className = "fs-5 text-truncate ";
-        h5.addEventListener("click", event => {
+        h5.addEventListener("click", (event) => {
           window.location.assign("./AlbumPage.html?albumId=" + album.id);
         });
         const name = document.createElement("a");
         name.innerText = album.artist.name;
-        name.className = "link-underline-secondary link-underline-opacity-0 link-underline-opacity-75-hover text-secondary fw-bold";
+        name.className =
+          "link-underline-secondary link-underline-opacity-0 link-underline-opacity-75-hover text-secondary fw-bold";
         name.href = "./artists.html?artistId=" + album.artist.id;
 
         imgContainer.append(img, btnPlay);
@@ -267,7 +274,7 @@ const cardsAlbum = (arrAlbums, container) => {
             break;
         }
       })
-      .catch(err => alert(err));
+      .catch((err) => alert(err));
   });
 };
 
@@ -278,7 +285,7 @@ window.addEventListener("DOMContentLoaded", function () {
   randomAlbum = Math.round(Math.random() * (albumAnnunci.length - 1));
   // ************************************album di annuncio del momento*******************************
   fetch("https://deezerdevs-deezer.p.rapidapi.com/album/" + albumAnnunci[randomAlbum], options)
-    .then(resp => {
+    .then((resp) => {
       if (resp.ok) {
         // restituiamo il dato convertito in array da JSON
         return resp.json();
@@ -286,14 +293,14 @@ window.addEventListener("DOMContentLoaded", function () {
         throw `Errore ${resp.status} : ${resp.statusText} `;
       }
     })
-    .then(album => {
+    .then((album) => {
       console.log(album);
       const annunci = document.getElementById("annunci");
       // dal array di canzoni prendo una a caso tra le prime 3 (non sappiamo se le top 50 canzoni sono veramente 50)
 
       const imgAnnunci = document.getElementById("imgAnnunci");
       imgAnnunci.setAttribute("src", album.cover_big);
-      imgAnnunci.addEventListener("click", event => {
+      imgAnnunci.addEventListener("click", (event) => {
         window.location.assign("./AlbumPage.html?albumId=" + album.id);
       });
       const infoAnnunci = document.createElement("div");
@@ -305,7 +312,7 @@ window.addEventListener("DOMContentLoaded", function () {
       h2.innerText = album.title;
       h2.className = "text-truncate";
       h2.style.maxWidth = "300px";
-      h2.addEventListener("click", event => {
+      h2.addEventListener("click", (event) => {
         window.location.assign("./AlbumPage.html?albumId=" + album.id);
       });
 
@@ -395,7 +402,7 @@ window.addEventListener("DOMContentLoaded", function () {
       infoAnnunci.append(spanAnnunci, h2, artistName, p2, containerBtn);
       annunci.appendChild(infoAnnunci);
     })
-    .catch(err => alert(err));
+    .catch((err) => alert(err));
 
   cardsArtist(artistiPopolari, "#perTe");
   cardsAlbum(albumsArtistaPref, "#artistaPref");
